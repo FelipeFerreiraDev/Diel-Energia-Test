07bde6dbfc6a95df907b6ff74603050551ac3eb9
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2022-10-19 15:28:44 -0300

    feat: created migration tasks

---
+++ backend/src/shared/infra/typeorm/migrations/1666203434106-CreateTasks.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateTasks1666203434106 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'tasks',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'title',
+            type: 'varchar'
+          },
+          {
+            name: 'description',
+            type: 'varchar'
+          },
+          {
+            name: 'date',
+            type: 'timestamp'
+          },
+          {
+            name: 'runtime',
+            type: 'varchar'
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+          }
+        ]
+      })
+    )
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('tasks')
+  }
+}
