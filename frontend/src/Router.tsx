0b42b44982d880823bf72fae0f9adf780122a2f8
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2022-10-22 20:00:26 -0300

    feat: updated routes

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Cadastrar from './pages/Cadastrar';
 import Home from './pages/Home';
+import ListTasks from './pages/ListTasks';
 import Login from './pages/Login';
 
 export default function Router() {
@@ -9,6 +10,7 @@ export default function Router() {
       <Route path="/" element={<Home />} />
       <Route path="/cadastrar" element={<Cadastrar />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/listtasks" element={<ListTasks />} />
     </Routes>
   );
 }
