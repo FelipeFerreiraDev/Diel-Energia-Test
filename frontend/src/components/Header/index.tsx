ab7dcaef61b43d82a3cdbef57fe0550acb490c2d
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2022-10-23 16:23:42 -0300

    feat: add nav header

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import style from './styles.module.scss';
 
 export default function Header() {
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    setToken(localStorage.getItem('token') || '');
+  }, []);
+
+  function handleLogout() {
+    localStorage.removeItem('token');
+    setToken('');
+  }
+
   return (
     <header className={style.header}>
       <Link to="/">
@@ -11,9 +23,23 @@ export default function Header() {
           </h1>
         </div>
       </Link>
-      <Link to="/login" className={style.login}>
-        login
-      </Link>
+      {token ? (
+        <div className={style.menu}>
+          <Link to="/list-tasks">Listar tarefas</Link>
+          <Link to="/cadastrar-task">Cadastrar tarefa</Link>
+        </div>
+      ) : null}
+
+      {token ? (
+        // eslint-disable-next-line react/jsx-no-bind
+        <Link to="/login" className={style.login} onClick={handleLogout}>
+          logout
+        </Link>
+      ) : (
+        <Link to="/login" className={style.login}>
+          login
+        </Link>
+      )}
     </header>
   );
 }
