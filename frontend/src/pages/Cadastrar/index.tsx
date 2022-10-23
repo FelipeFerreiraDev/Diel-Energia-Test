1304f6a6aa8f7756c9698d80dacdf459d02951a2
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2022-10-23 08:31:40 -0300

    feat: connected api user create

--- frontend/src/pages/Cadastrar/index.tsx
+++ frontend/src/pages/Cadastrar/index.tsx
@@ -1,19 +1,44 @@
+import { useState } from 'react';
 import TextInput from '../../components/TextInput';
+import api from '../../services/api';
 import style from './styles.module.scss';
 
 export default function Cadastrar() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmationPasssword, setConfirmationPassword] = useState('');
+
+  async function handleCreateAccount() {
+    const a = await api.post('/users', {
+      name,
+      email,
+      password,
+    });
+  }
+
   return (
     <div className={style.container}>
       <h1 className={style.title}>Cadastrar</h1>
 
-      <form action="" method="post">
+      <form className={style.form}>
         <div className={style.flexColumn}>
           Nome:
-          <TextInput name="nome" placeholder="Digite seu nome" type="text" />
+          <TextInput
+            name="nome"
+            placeholder="Digite seu nome"
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
         <div className={style.flexColumn}>
           Endereço de e-mail:
-          <TextInput name="email" placeholder="Digite seu e-mail" type="text" />
+          <TextInput
+            name="email"
+            placeholder="Digite seu e-mail"
+            type="text"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div className={style.flexColumn}>
           Sua senha:
@@ -21,14 +46,21 @@ export default function Cadastrar() {
             name="password"
             placeholder="Digite sua senha"
             type="password"
+            onChange={(e) => setPassword(e.target.value)}
           />
           <TextInput
             name="password"
             placeholder="Confirme sua senha"
             type="password"
+            onChange={(e) => setConfirmationPassword(e.target.value)}
           />
         </div>
-        <button type="button" className={style.button}>
+        <button
+          type="button"
+          className={style.button}
+          // eslint-disable-next-line @typescript-eslint/no-misused-promises
+          onClick={handleCreateAccount}
+        >
           Confirmar
         </button>
       </form>
