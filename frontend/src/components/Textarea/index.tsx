315364c09bb0b9e825e815b6a43ae7936679076f
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2022-10-23 16:24:17 -0300

    feat: add type in components

--- frontend/src/components/Textarea/index.tsx
+++ frontend/src/components/Textarea/index.tsx
@@ -1,12 +1,22 @@
 import style from './styles.module.scss';
 
-interface TextareaProps {
+interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
   name: string;
   placeholder?: string;
 }
 
-export default function Textarea({ name, placeholder }: TextareaProps) {
+export default function Textarea({
+  name,
+  placeholder,
+  ...rest
+}: TextareaProps) {
   return (
-    <textarea name={name} placeholder={placeholder} className={style.input} />
+    <textarea
+      name={name}
+      placeholder={placeholder}
+      className={style.input}
+      {...rest}
+    />
   );
 }
