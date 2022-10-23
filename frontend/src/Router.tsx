1da05343ba2eafeacff3a268c1e6cab30c2eb658
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2022-10-23 16:25:26 -0300

    feat: add all pages in router

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Cadastrar from './pages/Cadastrar';
 import CadastrarTask from './pages/Cadastrar Tasks';
+import EditarTask from './pages/Editar Tasks';
 import Home from './pages/Home';
 import ListTasks from './pages/ListTasks';
 import Login from './pages/Login';
@@ -13,6 +14,7 @@ export default function Router() {
       <Route path="/login" element={<Login />} />
       <Route path="/list-tasks" element={<ListTasks />} />
       <Route path="/cadastrar-task" element={<CadastrarTask />} />
+      <Route path="/editar-task/:task_id" element={<EditarTask />} />
     </Routes>
   );
 }
